bdccaf8aeb4ae3af99371322043146e27dc94361
Yaoyao Hu <shady030314@gmail.com>
2024-06-28 21:53:22 +0800

    .h files to implement

---
+++ include/argparser.h
@@ -0,0 +1,9 @@
+#ifndef ARGPARSER_H
+#define ARGPARSER_H
+
+#ifndef FOE_H
+#define FOE_H
+#include <foe.h>
+#endif
+
+#endif
---
+++ include/bit.h
@@ -0,0 +1,11 @@
+#ifndef BIT_H
+#define BIT_H
+
+#ifndef ARGPARSER_H
+#define ARGPARSER_H
+#include <argparser.h>
+#endif
+
+void bits(char* str);
+
+#endif
